5498cc9af44b3764297653709500b3cd6e289041
kostanikolov <kosta94@mail.bg>
2019-07-11 20:26:06 +0300

    modified files

--- 01-RestServicesAndAjax/exercise/01.BUS-STOP/app.js
+++ 01-RestServicesAndAjax/exercise/01.BUS-STOP/app.js
@@ -1,3 +1,33 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopId = Number(document.getElementById('stopId').value);
+    const busesList = document.getElementById('buses');
+    busesList.innerHTML = '';
+
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            const busStop = JSON.parse(this.responseText);
+            const buses = busStop.buses;
+
+            setStopName(busStop.name);
+            for (const bus in buses) {
+                const listItem = document.createElement('li');
+                listItem.textContent = `Bus ${bus} arrives in ${buses[bus]}`;
+                busesList.appendChild(listItem);
+            }
+        } else if (this.status === 401) {
+            setStopName('Error');
+        }
+    };
+
+    if (Number.isInteger(stopId)) {
+        req.open('GET', `https://judgetests.firebaseio.com/businfo/${stopId}.json `, true);
+        req.send();
+    } else {
+        setStopName('Error');
+    }
+
+    function setStopName(name) {
+        document.getElementById('stopName').textContent = name;
+    }
 }
--- 01-RestServicesAndAjax/exercise/02.BUS-SCHEDULE/app.js
+++ 01-RestServicesAndAjax/exercise/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,27 @@
 function solve() {
+    let baseUrl = 'https://judgetests.firebaseio.com/schedule/';
+    let currentId = 'depot';
+    let currentStop = '';
 
     function depart() {
-        console.log('Depart TODO...');
+        let url = baseUrl + currentId + '.json';
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                document.querySelector('.info').textContent = `Next stop ${data.name}`;
+                currentStop = data.name;
+                currentId = data.next;
+            });
+
+        document.getElementById('depart').disabled = true;
+        document.getElementById('arrive').disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        document.querySelector('.info').textContent = `Arriving at ${currentStop}`;
+        document.getElementById('depart').disabled = false;
+        document.getElementById('arrive').disabled = true;
     }
 
     return {
