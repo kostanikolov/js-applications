04884250335e00fd458636940c9a278406018294
kostanikolov <kosta94@mail.bg>
2019-08-01 16:39:17 +0300

    modified files

---
+++ 08-UniEntExamPrep/controllers/homeController.js
@@ -0,0 +1,31 @@
+const homeController = function () {
+
+    const getHome = async function (context) {
+        const loggedIn = storage.getData('userInfo') !== null;
+
+        if (loggedIn) {
+            const username = JSON.parse(storage.getData('userInfo')).username;
+            context.loggedIn = loggedIn;
+            context.username = username;
+
+            try {
+                const response = await eventModel.getAllEvents();
+                context.events = await response.json();
+            } catch(e) {
+                console.log(e);
+            }
+        }
+
+        context.loadPartials({
+            header: "../views/common/header.hbs",
+            footer: "../views/common/footer.hbs",
+            eventView: "../views/event/eventView.hbs",
+        }).then(function () {
+            this.partial('../views/home/homePage.hbs');
+        });
+    };
+
+    return {
+        getHome
+    }
+}();
---
+++ 08-UniEntExamPrep/helpers/storage.js
@@ -0,0 +1,31 @@
+const storage = function () {
+    const appKey = "kid_HJMS6dTGH";
+    const appSecret = "05a49ebb28d74e70b2c181d8a9d7d792";
+
+    const getData = function (key) {
+        return localStorage.getItem(key + appKey);
+    };
+
+    const saveData = function (key, value) {
+        localStorage.setItem(key + appKey, JSON.stringify(value));
+    };
+
+    const saveUser = function (data) {
+        saveData("userInfo", data);
+        saveData("authToken", data._kmd.authtoken);
+    };
+
+    const deleteUser = function () {
+        localStorage.removeItem("userInfo" + appKey);
+        localStorage.removeItem("authToken" + appKey);
+    };
+
+    return {
+        getData,
+        saveData,
+        saveUser,
+        deleteUser,
+        appKey,
+        appSecret
+    }
+}();
